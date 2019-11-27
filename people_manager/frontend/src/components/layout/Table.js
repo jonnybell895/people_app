c58fefdd7ed4584ca90316d978c27c9ee78613c2
jonnybell895 <jonnybell895@gmail.com>
2019-11-27 22:57:51 +0000

    Add checkboxes to table

--- people_manager/frontend/src/components/layout/Table.js
+++ people_manager/frontend/src/components/layout/Table.js
@@ -12,12 +12,14 @@ const Table = ({ data }) =>
       <table className="table is-striped">
         <thead>
           <tr>
+            <th>Marked</th>
             {Object.entries(data[0]).map(el => <th key={key(el)}>{el[0].charAt(0).toUpperCase() + el[0].slice(1)}</th>)}
           </tr>
         </thead>
         <tbody>
           {data.map(el => (
             <tr key={el.id}>
+              <td><input type="checkbox" id={el.id} /></td>
               {Object.entries(el).map(el => <td key={key(el)}>{el[1]}</td>)}
             </tr>
           ))}
